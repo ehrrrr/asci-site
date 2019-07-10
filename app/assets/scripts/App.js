63653722327be14c514712527082a7d256785075
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-07-10 20:07:52 +0300

    Compleated the reveal on scroll behavior. Animation for the items in product section.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll();
+// new RevealOnScroll($('.product-item'), '85%');
